0bd528ac39238792bf23e86bc90c014a89ce4e81
Serena Yao <105682904+SerenaYaoo@users.noreply.github.com>
2026-09-11 19:28:29 -0400

    LC-two-sum [Runtime: 0 ms; Memory: 20.6 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
+        seen = dict()
         for i in range(len(nums)):
-            current_char = nums[i]
-            wanted = target - current_char
-            for j in range(i+1, len(nums)):
-                if nums[j] == wanted:
-                    return [i, j]
+            current_number = nums[i]
+            wanted = target - current_number
+            if wanted in seen:
+                return [i, seen[wanted]]
+            seen[current_number] = i
         return []
-            
+
                 
