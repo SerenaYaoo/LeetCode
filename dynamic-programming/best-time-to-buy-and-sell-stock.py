eac4d36c41bb5348798ff97cafef48303896b5c6
Serena Yao <105682904+SerenaYaoo@users.noreply.github.com>
2026-09-15 16:40:31 -0400

    LC-best-time-to-buy-and-sell-stock [Runtime: 112 ms; Memory: 28.7 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock.py
+++ dynamic-programming/best-time-to-buy-and-sell-stock.py
@@ -6,8 +6,7 @@ class Solution:
         for price in prices:
             if price < min_price:
                 min_price = price
-            elif price - min_price > max_profit:
-                max_profit = price - min_price
+            max_profit = max(max_profit, price - min_price)
         return max_profit
             
         
